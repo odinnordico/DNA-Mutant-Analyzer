c89c678198c45acdc04ab55a0579292971c791f5
Diego Alfonso <odin.nordico90@gmail.comwq>
2021-04-25 15:40:32 -0500

    Changed test to a it-type test and added readme and heroku config

--- README.md
+++ README.md
@@ -1,2 +1,121 @@
 # DNA-Mutant-Analyzer
-Servicio que detecta si un humano es mutante basándose en su secuencia de ADN.
+
+Service for mutant/human detection based on their DNA sequence represented in a matrix type
+
+## Requisites
+
+### Build management
+
+`Gradle 6`
+
+### Database
+
+A PostgreSQL database, si far it si set in `postgres/public` schema bit it can ber changed in
+the `application.properties` file
+
+For local/testing purposes, the Database must be running in local with default port and user /
+password:
+
+#### Local
+
+* **URL:** jdbc:postgresql://localhost:5432/postgres
+* **Username:** postgres
+* **Password:** postgres
+
+For cloud environments, the `ENV` variables must set with the configuration
+
+* **URL:** `DATABASE_URL` _i.e.: postgres://user:pass@cloud-service:5432/database_
+* **Username:** `DATABASE_USER` _i.e.: user_
+* **Password:** `DATABASE_PASS` _i.e.: password_
+
+## Running the service
+
+### Local
+
+To run the service in _local_ environment, you can run the command:
+
+```shell
+spring_profiles_active=local ./gradlew clean build bootRun
+```
+
+where `spring_profiles_active` set the `env` variable to set the active profile, this case `local`
+
+### Cloud
+
+To run the service in _cloud_ environment, you can run the command:
+
+```bash
+java -jar DNA-Mutant-Analyzer-0.0.1-SNAPSHOT.jar
+```
+
+where `DNA-Mutant-Analyzer-0.0.1-SNAPSHOT.jar` is the jar build with `gradle` and can be found
+in `build/libs` and it needs to have the _Database env variables_ set
+
+## Using the service
+
+### Analyze mutant
+
+To analyze a DNA sequence if it is mutant or not, it is necessary to use a `POST` request with the
+following:
+
+#### Request
+
+`POST`: `{server}:{port}/mutant` _i.e.: http://localhost:8080/mutant_
+
+#### Headers
+
+* `Content-Type: application/json`
+
+#### Body
+
+```json
+{
+  "dna": [
+    "ATGCGA",
+    "CAGTGC",
+    "TTATGT",
+    "AGAAGG",
+    "CCCCTA",
+    "TCACTG"
+  ]
+}
+```
+
+#### Response
+
+* `true`
+  OR
+* `false`
+
+where `true`means that the DNA sequence is from a mutant and `false` means that the DNA sequence is
+from a human
+
+### Retrieve stats
+
+The stats retrieve the information of how many DNA sequences has been analyzed with result for _
+mutant_ and how many with result for _human_, you need to use a `GET` request to do it with
+following:
+
+#### Request
+
+`GET`: `{server}:{port}/stats` _i.e.: http://localhost:8080/stats_
+
+#### Headers
+
+* `Content-Type: application/json`
+
+#### Response
+
+```json
+{
+  "count_mutant_dna": 1,
+  "count_human_dna": 1,
+  "ratio": 1.5
+}
+```
+
+where
+
+* `count_mutant_dna` is the number of DNA analyzed with result of _mutant_
+* `count_human_dna` is the number of DNA analyzed with result of _human_
+* `ratio` is the ration of _mutant_ and _humans_ analyzed
---
+++ src/test/java/com/meli/test/dnamutantanalyzer/controllers/DNAAnalyzerControllerTest.java
@@ -0,0 +1,153 @@
+package com.meli.test.dnamutantanalyzer.controllers;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.when;
+
+import com.meli.test.dnamutantanalyzer.gateway.persistence.entity.StatsEntity;
+import com.meli.test.dnamutantanalyzer.gateway.persistence.repository.StatRepository;
+import com.meli.test.dnamutantanalyzer.model.CharMatrix;
+import com.meli.test.dnamutantanalyzer.model.DNARequest;
+import com.meli.test.dnamutantanalyzer.model.Stat;
+import com.meli.test.dnamutantanalyzer.services.DNAAnalyzerService;
+import com.meli.test.dnamutantanalyzer.services.DNAStatsService;
+import com.meli.test.dnamutantanalyzer.services.MatrixProcessor;
+import com.meli.test.dnamutantanalyzer.services.impl.CharMatrixProcessorService;
+import com.meli.test.dnamutantanalyzer.services.impl.DNAMutantAnalyzerService;
+import com.meli.test.dnamutantanalyzer.services.impl.DNAMutantStatsService;
+import java.util.Arrays;
+import java.util.Optional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+@ExtendWith(MockitoExtension.class)
+class DNAAnalyzerControllerTest {
+
+  @Mock
+  private StatRepository statRepository;
+  @Spy
+  private MatrixProcessor charMatrixProcessorService;
+  @Spy
+  private DNAStatsService dnaMutantStatsService;
+  @Spy
+  private DNAAnalyzerService dnaMutantAnalyzerService;
+  private DNAAnalyzerController dnaAnalyzerController;
+
+
+  @BeforeEach
+  void setUp() {
+    charMatrixProcessorService = new CharMatrixProcessorService();
+    dnaMutantStatsService = new DNAMutantStatsService(statRepository);
+    dnaMutantAnalyzerService = new DNAMutantAnalyzerService(statRepository,
+        charMatrixProcessorService, dnaMutantStatsService);
+
+    dnaAnalyzerController = new DNAAnalyzerController(dnaMutantAnalyzerService,
+        dnaMutantStatsService);
+  }
+
+  @Test
+  void testGetStats() {
+    when(statRepository.findAll()).thenReturn((Iterable<StatsEntity>) Arrays
+        .asList(
+            StatsEntity.builder().isMutant(true).build(),
+            StatsEntity.builder().isMutant(false).build()));
+    Stat stat = dnaAnalyzerController.getStats().getBody();
+    assertEquals(1, stat.getCountHumanDNA());
+    assertEquals(1, stat.getCountMutantDNA());
+    assertEquals(1, stat.getRatio());
+  }
+
+  @Test
+  void testGetAnaliseDNAMutantVertical() {
+    when(statRepository.findById(anyInt())).thenReturn(Optional.empty());
+    final DNARequest dnaRequest = DNARequest.builder()
+        .dna(new String[]{"AACTCA", "ATCCGT", "ACCGGC", "AGCAAG", "AATTAA", "ATGCTT"}).build();
+    assertTrue(dnaAnalyzerController.analiseDNA(dnaRequest).getBody());
+  }
+
+  @Test
+  void testGetAnaliseDNAMutantHorizontal() {
+    when(statRepository.findById(anyInt())).thenReturn(Optional.empty());
+    final DNARequest dnaRequest = DNARequest.builder()
+        .dna(new String[]{"AAAAAA", "ATCGAT", "CCCCTG", "TCGATC", "CGGAAT", "ATCGAT"}).build();
+    assertTrue(dnaAnalyzerController.analiseDNA(dnaRequest).getBody());
+  }
+
+  @Test
+  void testGetAnaliseDNAMutantDiagonalRight() {
+    when(statRepository.findById(anyInt())).thenReturn(Optional.empty());
+    final DNARequest dnaRequest = DNARequest.builder()
+        .dna(new String[]{"AATGGG", "TACTAT", "TCACTG", "CCGATT", "CGGATT", "TTCGAC"}).build();
+    assertTrue(dnaAnalyzerController.analiseDNA(dnaRequest).getBody());
+  }
+
+  @Test
+  void testGetAnaliseDNAMutantDiagonalLeft() {
+    when(statRepository.findById(anyInt())).thenReturn(Optional.empty());
+    final DNARequest dnaRequest = DNARequest.builder()
+        .dna(new String[]{"GGGTAA", "TATCAT", "GTCACT", "TTAGCC", "TTAGGC", "CAGCTT"}).build();
+    assertTrue(dnaAnalyzerController.analiseDNA(dnaRequest).getBody());
+  }
+
+  @Test
+  void testGetAnaliseDNAMutantHVlCross() {
+    when(statRepository.findById(anyInt())).thenReturn(Optional.empty());
+    final DNARequest dnaRequest = DNARequest.builder()
+        .dna(new String[]{"ATCGAT", "CGATCG", "TCAAAA", "TAGATA", "CGAACG", "TAGATA"}).build();
+    assertTrue(dnaAnalyzerController.analiseDNA(dnaRequest).getBody());
+  }
+
+  @Test
+  void testGetAnaliseDNAMutantDiagonalX() {
+    when(statRepository.findById(anyInt())).thenReturn(Optional.empty());
+    final DNARequest dnaRequest = DNARequest.builder()
+        .dna(new String[]{"ATCGAT", "CGATCG", "TCAAAA", "TAGATA", "CGATAG", "TAGCTA"}).build();
+    assertTrue(dnaAnalyzerController.analiseDNA(dnaRequest).getBody());
+  }
+
+  @Test
+  void testGetAnaliseDNAMutantVerticalDiagonal() {
+    when(statRepository.findById(anyInt())).thenReturn(Optional.empty());
+    final DNARequest dnaRequest = DNARequest.builder()
+        .dna(new String[]{"ATCGAT", "CGATCG", "TCGAAC", "TAGATA", "CGAACG", "TAGATA"}).build();
+    assertTrue(dnaAnalyzerController.analiseDNA(dnaRequest).getBody());
+  }
+
+  @Test
+  void testGetAnaliseDNAMutantIsHuman() {
+    when(statRepository.findById(anyInt())).thenReturn(Optional.empty());
+    final DNARequest dnaRequest = DNARequest.builder()
+        .dna(new String[]{"ATCGAT", "GCTAGC", "TCGATC", "AGCTAG", "TCGATC", "AGCTAG"}).build();
+    assertFalse(dnaAnalyzerController.analiseDNA(dnaRequest).getBody());
+  }
+
+  @Test
+  void testGetAnaliseDNAMutantIsNotHumanSaved() {
+    when(statRepository.findById(anyInt()))
+        .thenReturn(Optional.of(StatsEntity.builder().isMutant(true).build()));
+    final DNARequest dnaRequest = DNARequest.builder()
+        .dna(new String[]{"ATCGAT", "GCTAGC", "TCGATC", "AGCTAG", "TCGATC", "AGCTAG"}).build();
+    assertTrue(dnaAnalyzerController.analiseDNA(dnaRequest).getBody());
+  }
+
+  @Test
+  void testGetAnaliseDNAMalformedDNA() {
+    final DNARequest dnaRequest = DNARequest.builder()
+        .dna(new String[]{"A", "GCTAGC", "TCGATC", "AGCTAG", "TCGATC", "AGCTAG"}).build();
+    ResponseEntity<Boolean> responseEntity = dnaAnalyzerController.analiseDNA(dnaRequest);
+    assertEquals(HttpStatus.FORBIDDEN, responseEntity.getStatusCode());
+  }
+
+}
